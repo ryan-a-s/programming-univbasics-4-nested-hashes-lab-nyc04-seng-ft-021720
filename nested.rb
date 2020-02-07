6179480713d5aca98ad3435d691093ef93cf8c9b
Ryan Seit <ryan@ryanseit.com>
2020-02-07 20:30:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
         }
      }
 
-	programmer_hash = [:yukihiro_matsumoto => "Test"]
+	programmer_hash = [yukihiro_matsumoto: "Test"]
 	programmer_hash[:yukihiro_matsumoto]
 
 end
