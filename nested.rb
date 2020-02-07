a9f18c6f90d9353e891e78868787ccc1cda53507
Ryan Seit <ryan@ryanseit.com>
2020-02-07 20:44:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
 
 	hash = {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}
 	programmer_hash.merge!(hash)
-	return programmer_hash[:yukihiro_matsumoto]
+	hash
 
 end
 
